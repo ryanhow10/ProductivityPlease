2a29a907fecce4ac65c6e797afc0e3cd10de660b
Ryan How <ryanhow1999@yahoo.ca>
2020-02-16 17:30:09 -0500

    get all tasks route completed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const Task = require("./../models/Task");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
+    Task.find((err, data) => {
+        if(!err){
+            res.json(data);
+        } else{
+            res.send(err);
+        }
+    });
 });
 
 module.exports = router;
