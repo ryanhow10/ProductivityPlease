c04654ec9cf05643d912c5ffe2f10b3b32afc09c
Ryan How <ryanhow1999@yahoo.ca>
2020-02-16 17:35:55 -0500

    get specifc task by id

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Task = require("./../models/Task");
 const router = express.Router();
 
+//Get all tasks
 router.get("/", (req, res) => {
     Task.find((err, data) => {
         if(!err){
@@ -11,5 +12,20 @@ router.get("/", (req, res) => {
         }
     });
 });
+
+//Get spcific task
+router.get("/:taskId", (req, res) => {
+    Task.findById(req.params.taskId, (err, data) => {
+        if(!err){
+            res.json(data);
+        } else{
+            res.send(err);
+        }
+    }); 
+});
+
+//Add task
+
+//"Delete" task - set status to non-active
 
 module.exports = router;
