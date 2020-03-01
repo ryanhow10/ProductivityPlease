62a71443b309324b9e55bf0b68ae71cd2f854411
Ryan How <ryanhow1999@yahoo.ca>
2020-02-29 23:32:12 -0500

    refactored to async/await

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,73 +3,67 @@ const Task = require("./../models/Task");
 const router = express.Router();
 
 //Get all tasks
-router.get("/", (req, res) => {
-    Task.find({status: "active"}, (err, data) => {
-        if(!err){
-            res.json(data);
-        } else{
-            res.send(err);
-        }
-    });
+router.get("/", async (req, res) => {
+    try{
+        let tasks = await Task.find({status: "active"});
+        res.json(tasks);
+    } catch(err){
+        res.send({message: "Failed to retrieve tasks", error: err});
+    }
 });
 
 //Get spcific task
-router.get("/:taskId", (req, res) => {
-    Task.findById(req.params.taskId, (err, data) => {
-        if(!err){
-            res.json(data);
-        } else{
-            res.send(err);
-        }
-    }); 
+router.get("/:taskId", async (req, res) => {
+    try{
+        let task = await Task.findById(req.params.taskId);
+        res.json(task);
+    } catch(err){
+        res.send({message: "Failed to retrieve task", error: err});
+    }
 });
 
 //Add task
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     let task = new Task({
         title: req.body.title,
         description: req.body.description
     });
-    task.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.send(err);
-        });
+    try{
+        let newTask = await task.save();
+        res.json(newTask);
+    } catch(err){
+        res.send({message: "Failed to add new task", error: err});
+    }
 });
 
 //Update task (title and/or description)
-router.put("/:postId/edit", (req, res) => {
-    Task.findByIdAndUpdate(req.params.postId, {title: req.body.title, description: req.body.description}, (err, data) => {
-        if(!err){
-            res.json(data);
-        } else{
-            res.send(err);
-        }
-    });
+router.put("/:postId/edit", async (req, res) => {
+    try{
+        let updatedTask = await Task.findByIdAndUpdate(req.params.postId, {title: req.body.title, description: req.body.description});
+        res.json(updatedTask);
+    } catch(err){
+        res.send({message: "Failed to update task", error: err});
+    }
 });
 
 //Update task completion to true
-router.put("/:postId/:completed", (req, res) => {
-    Task.findByIdAndUpdate(req.params.postId, {completed: req.params.completed}, (err, data) => {
-        if(!err){
-            res.json(data);
-        } else{
-            res.send(err);
-        }
-    });
+router.put("/:postId/:completed", async (req, res) => {
+    try{
+        let completedTask = await Task.findByIdAndUpdate(req.params.postId, {completed: req.params.completed});
+        res.json(completedTask);
+    } catch(err){
+        res.send({message: "Failed to update task completion", error: err});
+    }
 });
 
 //"Delete" task - set status to non-active
-router.delete("/:postId", (req, res) => {
-    Task.findByIdAndUpdate(req.params.postId, {status: "non-active"}, (err, data) => {
-        if(!err){
-            res.json(data);
-        } else{
-            res.send(err);
-        }
-    });
+router.delete("/:postId", async (req, res) => {
+    try{
+        let deletedTask = await Task.findByIdAndUpdate(req.params.postId, {status: "non-active"});
+        res.json(deletedTask);
+    } catch(err){
+        res.send({message: "Failed to delete task", error: err});
+    }
 });
 
 module.exports = router;
